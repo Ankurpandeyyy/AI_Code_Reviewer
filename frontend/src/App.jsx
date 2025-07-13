c40950b1b7847289a7fee5373bacbb8c567c5f25
ankur <ankurkumar0924@gmail.com>
2025-07-13 12:55:09 +0530

    frontend and backend is working together properly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,20 +4,38 @@ import './App.css'
 import Editor from "react-simple-code-editor"
 import "prismjs/themes/prism-tomorrow.css"
 import prism from 'prismjs'
+import axios from 'axios'
+import Markdown from 'react-markdown'
+import rehypeHighlight from 'rehype-highlight'
+import "highlight.js/styles/github-dark.css";
 
 const App = () => {
 
-  const[code , setCode]= useState(`function sum() {
-     return 1+1 ;}`)
+  const[code , setCode]= useState(``)
   useEffect(() => {
     prism.highlightAll()
   } , [])
+
+  async function ReviewCode(){
+     const response = await axios.post('http://localhost:3000/ai/get-review' , {code})
+
+     setreview(response.data)
+  }
+
+  const[review , setreview] = useState(``)
+  
+  
+  function clearScreen(){
+  setCode(``);
+  setreview(``);
+  }
+
 
   return (
     <>
     <div className="bg-amber-50 flex justify-items-normal gap-1 justify-evenly">
       <div className="p-2 w-6/12 h-screen bg-black border-2 rounded-2xl">
-       <div className='h-11/12 w-12/12'>
+       <div className='h-[92%] w-full text-white overflow-auto'>
         <Editor
               value={code}
               onValueChange={code => setCode(code)}
@@ -33,9 +51,20 @@ const App = () => {
               }}
             />
        </div>
-       <button className= 'px-7 py-2.5 relative left-10/12 top-2 rounded-2xl font-bold bg-blue-300 cursor-pointer'>Review</button>
+       
+        <div className="flex justify-end mt-2 overflow-auto gap-3">
+          <button onClick={clearScreen} className=' px-7 py-2  rounded-2xl font-bold bg-blue-400 cursor-pointer overflow-auto'>Clear</button>
+    <button
+      onClick={ReviewCode}
+      className="px-7 py-2 rounded-2xl font-bold bg-blue-400 cursor-pointer overflow-auto"
+    >
+      Review
+    </button>
+  </div>
       </div>
-      <div className="p-2 w-6/12 h-screen bg-gray-500 border-2 rounded-2xl"></div>
+      <div className="p-2 w-6/12 h-screen bg-gray-400 border-2 rounded-2xl overflow-auto font-bold">
+      <Markdown rehypePlugins={[rehypeHighlight]}>{review}</Markdown>
+      </div>
     </div>
     </>
   )
